072cf5cb837e52693f3f2036efe84d7c64b80f08
sarahabbas10 <sarahabbas1418@gmail.com>
2021-10-11 17:14:06 +0300

    js-DOM lab

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // console.log(document.getElementById("b2"))
 
 
+
 function changeHead1() {
     console.log("fun1 ")
     document.getElementById('heading').style.fontSize =
@@ -43,4 +44,41 @@ function imgChange() {
     } else {
         document.getElementById('img').src = "http://www.javatpoint.com/images/javascript/javascript_logo.png"
     }
+}
+
+//Change an image when the mouse moves onto it, and then back to the original image when the mouse moves off of it.
+function mouseOver() {
+    console.log("img over");
+    document.getElementById('img').src = "js2.jpg"
+}
+
+function mouseOut() {
+    console.log("img out");
+    document.getElementById('img').src = "http://www.javatpoint.com/images/javascript/javascript_logo.png"
+}
+
+
+//Create a script which creates an alert on the page when a user right clicks on a speciﬁc image
+//document.getElementById('img').addEventListener('onmousedown', alert("you click on the img !!!"))
+
+//Create an alert on the page when a user enters data into a form.
+if (document.getElementById('name') == '' || document.getElementById('course') == '') {
+    alert("you enter ")
+}
+
+//Create an alert to let the user know when the page has loaded.
+window.addEventListener('onload', alert("Page is loaded"))
+
+
+
+//Highlight all paragraphs when the mouse moves over them (then revert back once it has moved past them).
+
+
+function allP() {
+
+    document.getElementById('paragraph').style.backgroundColor = "yellow";
+}
+
+function allP2() {
+    document.getElementById('paragraph').style.backgroundColor = "";
 }
